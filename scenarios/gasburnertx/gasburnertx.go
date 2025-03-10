bf56413b6b26cca89215d232f86845ccfa92b0c1
Minhyuk Kim <kimminhyuk1004@gmail.com>
2025-03-10 18:31:20 +0900

    Use rate.Limiter for controlling the throughput of rpc requests

--- scenarios/gasburnertx/gasburnertx.go
+++ scenarios/gasburnertx/gasburnertx.go
@@ -8,14 +8,16 @@ import (
 	"sync/atomic"
 	"time"
 
+	"golang.org/x/time/rate"
+
+	"github.com/ethpandaops/spamoor/scenariotypes"
+	"github.com/ethpandaops/spamoor/tester"
+	"github.com/ethpandaops/spamoor/txbuilder"
 	"github.com/ethpandaops/spamoor/utils"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethpandaops/spamoor/scenariotypes"
-	"github.com/ethpandaops/spamoor/tester"
-	"github.com/ethpandaops/spamoor/txbuilder"
 	"github.com/holiman/uint256"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
@@ -95,7 +97,6 @@ func (s *Scenario) Run(tester *tester.Tester) error {
 	txIdxCounter := uint64(0)
 	pendingCount := atomic.Int64{}
 	txCount := atomic.Uint64{}
-	startTime := time.Now()
 	var lastChan chan bool
 
 	s.logger.Infof("starting scenario: gasburnertx")
@@ -110,7 +111,19 @@ func (s *Scenario) Run(tester *tester.Tester) error {
 
 	s.logger.Infof("deployed gas burner contract at %v", s.gasBurnerContractAddr.String())
 
+	initialRate := rate.Limit(float64(s.options.Throughput) / float64(utils.SecondsPerSlot))
+	if initialRate == 0 {
+		initialRate = rate.Inf
+	}
+	limiter := rate.NewLimiter(initialRate, int(s.options.Throughput))
+
 	for {
+		if err := limiter.Wait(context.Background()); err != nil {
+			s.logger.Debugf("rate limited: %s", err.Error())
+			time.Sleep(100 * time.Millisecond)
+			continue
+		}
+
 		txIdx := txIdxCounter
 		txIdxCounter++
 
@@ -157,11 +170,6 @@ func (s *Scenario) Run(tester *tester.Tester) error {
 		count := txCount.Load() + uint64(pendingCount.Load())
 		if s.options.TotalCount > 0 && count >= s.options.TotalCount {
 			break
-		}
-		if s.options.Throughput > 0 {
-			for count/((uint64(time.Since(startTime).Seconds())/utils.SecondsPerSlot)+1) >= s.options.Throughput {
-				time.Sleep(100 * time.Millisecond)
-			}
 		}
 	}
 
