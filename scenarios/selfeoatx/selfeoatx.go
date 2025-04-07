64b0b8db8d221056879087872690a7a6f0b5cbd6
Kamil Chodoła <43241881+kamilchodola@users.noreply.github.com>
2025-04-07 18:05:57 +0200

    Add very important newline
    Signed-off-by: Kamil Chodoła <43241881+kamilchodola@users.noreply.github.com>

--- scenarios/selfeoatx/selfeoatx.go
+++ scenarios/selfeoatx/selfeoatx.go
@@ -13,6 +13,7 @@ import (
 	"golang.org/x/time/rate"
 	"gopkg.in/yaml.v3"
 
+	
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/holiman/uint256"
