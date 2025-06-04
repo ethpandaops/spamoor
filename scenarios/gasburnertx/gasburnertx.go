2b9687ec9b44afdf76a0be6ca1e9f9121f88f6cc
pk910 <philipp@pk910.de>
2025-06-04 14:50:53 +0200

    default to low througput when neither throughput nor total_count are set

--- scenarios/gasburnertx/gasburnertx.go
+++ scenarios/gasburnertx/gasburnertx.go
@@ -120,7 +120,7 @@ func (s *Scenario) Init(options *scenariotypes.ScenarioOptions) error {
 	}
 
 	if s.options.TotalCount == 0 && s.options.Throughput == 0 {
-		return fmt.Errorf("neither total count nor throughput limit set, must define at least one of them (see --help for list of all flags)")
+		s.options.Throughput = 1
 	}
 
 	return nil
--- scenarios/setcodetx/setcodetx.go
+++ scenarios/setcodetx/setcodetx.go
@@ -139,7 +139,7 @@ func (s *Scenario) Init(options *scenariotypes.ScenarioOptions) error {
 	}
 
 	if s.options.TotalCount == 0 && s.options.Throughput == 0 {
-		return fmt.Errorf("neither total count nor throughput limit set, must define at least one of them (see --help for list of all flags)")
+		s.options.Throughput = 1
 	}
 
 	s.delegatorSeed = make([]byte, 32)
