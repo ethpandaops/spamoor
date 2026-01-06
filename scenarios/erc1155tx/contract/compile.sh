4a58b7df6a0ae90dc134a0b8c3f62d1fc5452a1d
pk910 <philipp@pk910.de>
2026-01-07 00:13:35 +0100

    cleanup

--- scenarios/erc1155tx/contract/compile.sh
+++ scenarios/erc1155tx/contract/compile.sh
@@ -5,5 +5,4 @@ source "$SCRIPT_DIR/../../../scripts/compile-contract.sh"
 cd $SCRIPT_DIR
 
 # NFT Tokens
-compile_contract "$(pwd)" 0.8.22 "--optimize --optimize-runs 200" TestToken721
 compile_contract "$(pwd)" 0.8.22 "--optimize --optimize-runs 200" TestToken1155
